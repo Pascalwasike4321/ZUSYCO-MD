f23b1bd1d9a95b8b807ff4cdf2d2f9905f5843ad
Pascalwasike4321 <pascalwasike4321@gmail.com>
2024-07-23 15:07:19 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,9 +16,9 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94715166712",
-  SUDO_NUMBERS : "94715166712,94719574492",
-  OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
+  OWNER_NUMBER: "254714480162",
+  SUDO_NUMBERS : "254714480162,94719574492",
+  OWNER_NAME: "『 PASCAL 』",
   BOT_NAME : "ＺＵＳＹＣＯ",
   ZUSYCO_FILE : "ZUSYCO-QR",
   LOGOS: {
